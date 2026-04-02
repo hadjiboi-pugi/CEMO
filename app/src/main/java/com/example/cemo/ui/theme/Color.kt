20390592b1e17537d6abfb77764f005a60aea474
hadjiboi-pugi <hadj4367@gmail.com>
2026-04-02 17:02:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cemo/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.cemo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryGreen = Color(0xFF00A841)
+val SecondaryGreen = Color(0xFFE8F5E9)
+val BackgroundGreen = Color(0xFFF1FAF3)
+val DarkGreen = Color(0xFF006400)
+val DarkBrown = Color(0xFF3D2717)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
